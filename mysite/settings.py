efce0c17f2b1a27e7168de6c40c62924a9860fdf
Divyansh Sharma <41879236+dsnum1@users.noreply.github.com>
2023-04-01 06:02:52 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,3 +141,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT=False
+SESSION_COOKIE_SECURE=False
+CSRF_COOKIE_SECURE=False
+
+    
